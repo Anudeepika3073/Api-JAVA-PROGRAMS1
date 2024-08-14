260370746820d8bc0733d1042953cc059ac71005
anudeepika.a <anudeepika.a@releaseowl.com>
2024-08-14 16:46:17 +0530

    DS projects

--- demo/src/main/java/com/example/demo/Controller/QueueController.java
+++ demo/src/main/java/com/example/demo/Controller/QueueController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Queue;
 
 @RestController
@@ -14,11 +15,13 @@ public class QueueController {
     @Autowired
     public QueueService queueService;
     @GetMapping("/add")
-    public void getElement(){
+    public ArrayList<Integer> getElement(){
       QueueService queueService1=new QueueService();
       queueService1.enqueue(3);
-      queueService1.Display();
-
+      queueService1.enqueue(5);
+      queueService1.dequeue();
+      ArrayList<Integer>al=queueService1.Display();
+      return al;
     }
 //    @GetMapping("/DUMMY")
 //    public int my(){
--- demo/src/main/java/com/example/demo/Service/QueueService.java
+++ demo/src/main/java/com/example/demo/Service/QueueService.java
@@ -2,40 +2,51 @@ package com.example.demo.Service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+
 @Service
 
 public class QueueService {
     Node front;
     Node rear;
 
-    public QueueService( ){
-        this.front=null;
-        this.rear=null;
+    public QueueService() {
+        this.front = null;
+        this.rear = null;
     }
+
     public void enqueue(int data) {
         Node newNode = new Node(data);
         if (rear == null) {
             front = rear = newNode;
             //System.out.println("hi");
-            front.next=rear;
+            front.next = rear;
         } else {
 
-            rear.next=newNode;
-            rear=rear.next;
+            rear.next = newNode;
+            rear = rear.next;
         }
 
     }
-    public void Display(){
-     while(front!=null)
-     {
-         System.out.println(front.data);
-         front=front.next;
-     }
+
+    public ArrayList<Integer> Display() {
+        ArrayList<Integer>al=new ArrayList<>();
+        while (front != null) {
+            al.add(front.data);
+            System.out.println(front.data);
+            front = front.next;
+        }
+        return al;
     }
 
+    public void dequeue() {
+        while (front != null) {
+            front = front.next;
+
+        }
+    }
 }
+
+
+
 
